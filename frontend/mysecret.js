f1d4160f1d24c2c8617da8ec943b5a477bc06d0e
nutpattara <itsnutp@gmail.com>
2019-12-12 17:35:58 +0700

    add temporary frontend site

---
+++ frontend/mysecret.js
@@ -0,0 +1,4 @@
+class AppData {}
+AppData.APPID = "BassinetEmbeddedLab";
+AppData.KEY = "WMuA2ANcimOaf81";
+AppData.SECRET = "7aTnk0NArBItN6vRMupIDNNAw";
---
+++ frontend/secret.js
@@ -0,0 +1,4 @@
+class AppData {}
+AppData.APPID = "YOUR_APP_ID";
+AppData.KEY = "YOUR_KEY";
+AppData.SECRET = "YOUR_SECRET";
